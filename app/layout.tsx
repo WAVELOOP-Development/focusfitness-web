df31bcc64c707c2a9db3a73c70774566ab746ff0
Navindu-Ashen <navindu158@gmail.com>
2025-07-15 01:35:18 +0530

    feat(meta-data): fixes meta data sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,40 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: "Focus Fitness",
-  description: "Your ultimate destination for achieving fitness excellence.",
-    generator: 'v0.dev'
+  title: {
+    template: 'Focus Fitness',
+    default: 'Focus Fitness - Your Ultimate Destination for Achieving Fitness Excellence',
+  },
+  description: "Sri Lanka's Smartest Fitness App – Personalized Plans, AI Coaching, and Powerful Gym Management Tools.",
+  keywords: ['Focus Fitness', 'Health', 'Wellness', 'Fitness', 'Personal Training', 'Nutrition'],
+  creator: 'Team Waveloop',
+  icons: {
+    icon: [
+      { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: '/icon-192.png',
+  },
+  openGraph: {
+    title: 'Focus Fitness',
+    description: "Sri Lanka's Smartest Fitness App – Personalized Plans, AI Coaching, and Powerful Gym Management Tools.",
+    url: 'https://focusfitness.waveloop.dev',
+    siteName: 'Focus Fitness',
+    images: [
+      {
+        url: 'https://focusfitness.waveloop.dev/logoE.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Focus Fitness Logo',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 export default function RootLayout({
@@ -30,6 +61,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${bebasNeue.variable} ${inter.variable}`}>
+    <head>
+         <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Focus Fitness",
+              "url": "https://focusfitness.waveloop.dev",
+              "logo": "https://focusfitness.waveloop.dev/logoE.jpg",
+              "description": "Focus Fitness is Sri Lanka's Smartest Fitness App – Personalized Plans, AI Coaching, and Powerful Gym Management Tools."
+            })
+          }}
+        />
+      </head>
       {" "}
       {/* Apply font variables to html */}
       <body>{children}</body>
